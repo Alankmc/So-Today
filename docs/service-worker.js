d5011ea1de23cbb2925bd0f11a0f9f82a6f58012
alankmc <alankmc@gmail.com>
2018-10-20 10:09:21 -0300

    Still trying to set up building

--- docs/service-worker.js
+++ docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.736e3599b3181f6eefcb3c2fdcfef851.js"
+  "/So-Today/precache-manifest.6ba329cb1981bbcda882297ab5e34c90.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/So-Today/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
