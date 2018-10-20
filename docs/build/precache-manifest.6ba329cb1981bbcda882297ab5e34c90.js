69c60947cf5cca05b60ec6264d7b53f408685555
alankmc <alankmc@gmail.com>
2018-10-20 10:04:44 -0300

    Changed serving folder

---
+++ docs/build/precache-manifest.6ba329cb1981bbcda882297ab5e34c90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/So-Today/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a94adaa4251087141cd8",
+    "url": "/So-Today/static/js/runtime~main.a94adaa4.js"
+  },
+  {
+    "revision": "a839c668998fba5b2dd6",
+    "url": "/So-Today/static/js/main.a839c668.chunk.js"
+  },
+  {
+    "revision": "6105a37ce77693a684b2",
+    "url": "/So-Today/static/js/1.6105a37c.chunk.js"
+  },
+  {
+    "revision": "a839c668998fba5b2dd6",
+    "url": "/So-Today/static/css/main.b810670e.chunk.css"
+  },
+  {
+    "revision": "55e64c986489aec0cd145c3149efe8b8",
+    "url": "/So-Today/index.html"
+  }
+];
