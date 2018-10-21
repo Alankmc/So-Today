4663f412ae0d122411c93e8bd6037e6b873fb7f3
alankmc <alankmc@gmail.com>
2018-10-21 12:47:51 -0300

    Made config pane

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import ActionText from '../action-text/ActionText';
-import GameButton from '../game-buttons/GameButtons';
-import './App.css';
 import CommentText from '../comment-text/CommentText';
+import ConfigPane from '../config-pane/ConfigPane';
+import './App.css';
 
 const VIKING_MASTER_CHANCE = 0.2;
 const QUESTION_MASTER_CHANCE = 0.1;
@@ -55,29 +55,10 @@ class App extends Component {
 
     return (
       <div id="root-app" className="App">
-        <div className="button-container">
-          <GameButton
-            label={{ on: 'I LOVE YELLING', off: 'shh we\'re at the library' }}
-            callback={this.handleParamClick}
-            type="no_library"
-            value={gameParams.no_library}
-          />
-          <GameButton
-            label={{ on: 'Linkedin master', off: 'Blushing introvert' }}
-            callback={this.handleParamClick}
-            type="icebreak"
-            value={gameParams.icebreak}
-          />
-          <GameButton
-            label={{ on: 'F*ck\'em up', off: 'Be nice to your friends, Timmy' }}
-            callback={this.handleParamClick}
-            type="violence"
-            value={gameParams.violence}
-          />
-        </div>
+        <ConfigPane gameParams={gameParams} callback={this.handleParamClick} />
         <ActionText gameParams={gameParams} clickTick={this.clickTickHandler} />
         {isQuestion ? (<div><span className="event-span">Player has become Questionmaster</span><br /></div>) : null}
-        {isViking ? (<div><span className="event-span">Player has become Vikingmaster</span><br /></div>) : null}
+        {isViking ? (<div><span className="event-span lower">Player has become Vikingmaster</span><br /></div>) : null}
         <CommentText shouldRender={tickOnce && Math.random() < COMMENTARY_CHANCE} />
       </div>
     );
